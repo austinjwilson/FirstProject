a2e0ae604a7bc94cf7572c009f068ee64f70a47b
Austin Wilson <austin@snapit.solutions>
2020-01-06 15:03:49 -0600

    Task 2 Day 4

---
+++ Week1Day4.py
@@ -0,0 +1,15 @@
+#Day 4 Task 2
+#add_up(4) = 10
+#add_up(13) = 91
+#add_up(600) = 180300
+list = [1, 2, 3, 4]
+Sum = sum(list)
+print(Sum)
+
+list = range(1 + 13)
+Sum = sum(list)
+print(Sum)
+
+list = range(1 + 600)
+Sum = sum(list)
+print(Sum)
